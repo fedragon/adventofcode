8863ce0e6576533b7876a62cc3d4fe24edd75356
Federico Ragona <federico.ragona@gmail.com>
2022-12-04 06:42:36 +0100

    Solve day 4

---
+++ go/day4/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Range struct {
+	Start, End int
+}
+
+func Parse(pair string) (*Range, error) {
+	tokens := strings.Split(pair, "-")
+	if len(tokens) != 2 {
+		return nil, fmt.Errorf("expected an assignment pair, but got %v", tokens)
+	}
+	start, err := strconv.Atoi(tokens[0])
+	if err != nil {
+		return nil, err
+	}
+	end, err := strconv.Atoi(tokens[1])
+	if err != nil {
+		return nil, err
+	}
+
+	return &Range{Start: start, End: end}, nil
+}
+
+func (r *Range) Overlaps(a *Range) bool {
+	if r.End < a.Start {
+		return false
+	} else if a.End < r.Start {
+		return false
+	}
+
+	return true
+}
+
+func (r *Range) FullyContains(a *Range) bool {
+	return r.Start <= a.Start && r.End >= a.End
+}
+
+func main() {
+	f, err := os.Open("../data/day4")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	var fullyContained int
+	var overlapping int
+	for scanner.Scan() {
+		line := scanner.Text()
+		pairs := strings.Split(line, ",")
+		left, err := Parse(pairs[0])
+		if err != nil {
+			panic(err)
+		}
+		right, err := Parse(pairs[1])
+		if err != nil {
+			panic(err)
+		}
+
+		if left.FullyContains(right) || right.FullyContains(left) {
+			fullyContained++
+		}
+
+		if left.Overlaps(right) {
+			overlapping++
+		}
+	}
+
+	fmt.Println("fully contained ranges", fullyContained)
+	fmt.Println("overlapping ranges", overlapping)
+}
---
+++ go/day4/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+func TestRange_Overlaps(t *testing.T) {
+	type args struct {
+		a *Range
+		b *Range
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"2-4, 5-7",
+			args{a: &Range{Start: 2, End: 4}, b: &Range{Start: 5, End: 7}},
+			false,
+		},
+		{
+			"5-7, 2-4",
+			args{a: &Range{Start: 5, End: 7}, b: &Range{Start: 2, End: 4}},
+			false,
+		},
+		{
+			"2-4, 4-6",
+			args{a: &Range{Start: 2, End: 4}, b: &Range{Start: 4, End: 6}},
+			true,
+		},
+		{
+			"2-4, 3-3",
+			args{a: &Range{Start: 2, End: 4}, b: &Range{Start: 3, End: 3}},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.args.a.Overlaps(tt.args.b); got != tt.want {
+				t.Errorf("Overlaps() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
