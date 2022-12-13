3812da7546d80dc6259ad59f22bf796baf10b183
Federico Ragona <federico.ragona@gmail.com>
2022-12-13 12:23:01 +0100

    Solve day 12

--- go/day12/main.go
+++ go/day12/main.go
@@ -4,123 +4,25 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"github.com/fedragon/adventofcode/common"
 	"math"
 	"os"
-	"sort"
 )
-
-var emptyNode = Node{}
 
 type Coord struct {
 	Row, Col int
 }
 
 type Node struct {
-	Value    rune
-	Coord    Coord
-	Distance int
-}
-
-func (n *Node) Visit(grid [][]Node) (int, bool) {
-	return visit(grid, *n, nil)
-}
-
-// neighboursIn returns a list of nodes sorted by elevationGain in descending order
-func (n *Node) neighboursIn(grid [][]Node) []Node {
-	var neighbours []Node
-
-	exists := func(c Coord) bool {
-		return c.Row > -1 && c.Row < len(grid) &&
-			c.Col > -1 && c.Col < len(grid[c.Row])
-	}
-
-	reachable := func(current, next rune) bool {
-		return (current == 'S' && next == 'a') ||
-			(current == 'z' && next == 'E') ||
-			next-current <= 1
-	}
-
-	move := func(row, col int) []Coord {
-		return []Coord{
-			{row - 1, col},
-			{row + 1, col},
-			{row, col - 1},
-			{row, col + 1},
-		}
-	}
-
-	gains := make(map[int][]Node)
-	for _, coord := range move(n.Coord.Row, n.Coord.Col) {
-		if exists(coord) {
-			if next := grid[coord.Row][coord.Col]; reachable(n.Value, next.Value) {
-				neighbours = append(neighbours, next)
-				elevationGain := int(next.Value - n.Value)
-				gains[elevationGain] = append(gains[elevationGain], next)
-			}
-		}
-	}
-
-	var keys []int
-	for k := range gains {
-		keys = append(keys, k)
-	}
-	sort.Sort(sort.Reverse(sort.IntSlice(keys)))
-
-	var sorted []Node
-	for _, k := range keys {
-		sorted = append(sorted, gains[k]...)
-	}
-
-	return sorted
-}
-
-func inVisited(visited []Node, n Node) bool {
-	for _, x := range visited {
-		if n.Value == x.Value && n.Coord.Row == x.Coord.Row && n.Coord.Col == x.Coord.Col {
-			return true
-		}
-	}
-
-	return false
-}
-
-func visit(grid [][]Node, start Node, visited []Node) (int, bool) {
-	if start == emptyNode {
-		return math.MaxInt, false
-	}
-
-	neighbours := start.neighboursIn(grid)
-	fmt.Printf("start: %v (%d, %d): %d, len(visited) = %d\n", start.Value, start.Coord.Row, start.Coord.Col, start.Distance, len(visited))
-
-	for i := range neighbours {
-		if neighbours[i].Value == 'E' {
-			neighbours[i].Distance = start.Distance + 1
-			return start.Distance + 1, true
-		}
-
-		if !inVisited(visited, neighbours[i]) {
-			neighbours[i].Distance = start.Distance + 1
-		}
-	}
-
-	for i := range neighbours {
-		if !inVisited(visited, neighbours[i]) {
-			if res, found := visit(grid, neighbours[i], append(visited, start)); found {
-				return res, true
-			}
-
-			visited = append(visited, neighbours[i])
-		}
-	}
-
-	return math.MaxInt, false
+	Coord Coord
+	Steps int
 }
 
 type Part1Solver struct{}
 
 func (ds *Part1Solver) Solve(scanner *bufio.Scanner) (int, error) {
-	var heightmap [][]Node
-	var start Node
+	var heightmap [][]rune
+	var start, end Coord
 
 	var rowIndex int
 	for scanner.Scan() {
@@ -131,57 +33,154 @@ func (ds *Part1Solver) Solve(scanner *bufio.Scanner) (int, error) {
 			continue
 		}
 
-		var row []Node
+		var row []rune
 		for colIndex, value := range line {
-			node := Node{
-				Value: value,
-				Coord: Coord{
-					Row: rowIndex,
-					Col: colIndex,
-				},
-				Distance: math.MaxInt,
-			}
-
+			v := value
 			if value == 'S' {
-				node.Distance = 0
-				start = node
+				start = Coord{Row: rowIndex, Col: colIndex}
+				v = 'a'
+			} else if value == 'E' {
+				end = Coord{Row: rowIndex, Col: colIndex}
+				v = 'z'
 			}
 
-			row = append(row, node)
+			row = append(row, v)
 		}
 		rowIndex++
 		heightmap = append(heightmap, row)
 	}
 
-	if steps, found := start.Visit(heightmap); found {
+	if steps, found := visit(heightmap, Node{start, 0}, end); found {
 		return steps, nil
 	}
 
-	return 0, errors.New("no path found")
+	return 0, errors.New("path not found")
+}
+
+type Part2Solver struct{}
+
+func (ds *Part2Solver) Solve(scanner *bufio.Scanner) (int, error) {
+	var heightmap [][]rune
+	var end Coord
+	var starts []Coord
+
+	var rowIndex int
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+
+		if len(line) == 0 {
+			continue
+		}
+
+		var row []rune
+		for colIndex, value := range line {
+			v := value
+			if value == 'S' || value == 'a' {
+				starts = append(starts, Coord{Row: rowIndex, Col: colIndex})
+				v = 'a'
+			} else if value == 'E' {
+				end = Coord{Row: rowIndex, Col: colIndex}
+				v = 'z'
+			}
+
+			row = append(row, v)
+		}
+		rowIndex++
+		heightmap = append(heightmap, row)
+	}
+
+	min := math.MaxInt
+	for _, start := range starts {
+		if steps, found := visit(heightmap, Node{start, 0}, end); found {
+			if steps < min {
+				min = steps
+			}
+		}
+	}
+
+	if min == math.MaxInt {
+		return 0, errors.New("no path found")
+	}
+
+	return min, nil
+}
+
+func alreadyQueued(xs []Node, c Coord) bool {
+	for _, x := range xs {
+		if x.Coord == c {
+			return true
+		}
+	}
+
+	return false
+}
+
+func inGrid(c Coord, maxRow, maxCol int) bool {
+	return c.Row > -1 && c.Row < maxRow &&
+		c.Col > -1 && c.Col < maxCol
+}
+
+func reachable(current, next rune) bool {
+	return next-current <= 1
+}
+
+func visit(heightmap [][]rune, start Node, end Coord) (int, bool) {
+	q := []Node{start}
+	visited := make(map[Coord]bool)
+	visited[start.Coord] = true
+
+	for len(q) > 0 {
+		curr := q[0]
+		q = q[1:]
+		coord := curr.Coord
+		visited[coord] = true
+
+		if coord == end {
+			return curr.Steps, true
+		}
+
+		value := heightmap[coord.Row][coord.Col]
+		neighbours := []Coord{
+			{coord.Row - 1, coord.Col},
+			{coord.Row + 1, coord.Col},
+			{coord.Row, coord.Col - 1},
+			{coord.Row, coord.Col + 1},
+		}
+
+		for _, n := range neighbours {
+			if inGrid(n, len(heightmap), len(heightmap[coord.Row])) &&
+				reachable(value, heightmap[n.Row][n.Col]) &&
+				!visited[n] &&
+				!alreadyQueued(q, n) {
+				q = append(q, Node{Coord: n, Steps: curr.Steps + 1})
+			}
+		}
+	}
+
+	return 0, false
 }
 
 func main() {
-	f, err := os.Open("../data/day12")
-	if err != nil {
-		panic(err)
-	}
+	f := common.Must(os.Open("../data/day12"))
 	defer f.Close()
-	scanner := bufio.NewScanner(f)
 
 	part1 := Part1Solver{}
-	solution, err := part1.Solve(scanner)
+	solution, err := part1.Solve(bufio.NewScanner(f))
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("solution for part 1", solution)
 
-	// part2 := PartXSolver{}
-	// solution, err = part2.Solve("../data/day12")
-	//
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//
-	// fmt.Println("solution for part 2", solution)
+	common.Must(f.Seek(0, 0))
+
+	part2 := Part2Solver{}
+	solution, err = part2.Solve(bufio.NewScanner(f))
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("solution for part 2", solution)
 }
